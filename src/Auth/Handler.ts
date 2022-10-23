37ebdc3682cf46e8f00f34bc5d2285f7cc61335b
Franco Peratta <franco.peratta@hotmail.com>
2022-10-23 14:08:19 -0300

    Hours of operation first draft

--- src/Auth/Handler.ts
+++ src/Auth/Handler.ts
@@ -1,34 +1,29 @@
 import { User } from "firebase/auth"
-import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore"
+import { doc, getDoc, setDoc } from "firebase/firestore"
 import { db } from "../firebase/firestore"
 
 const defaultShift = {
-  0: false,
-  1: false,
-  2: false,
-  3: false,
-  4: false,
-  5: false,
-  6: false,
-  7: false,
-  8: false,
-  9: true,
-  10: true,
-  11: true,
-  12: true,
-  13: false,
-  14: false,
-  15: false,
-  16: true,
-  17: true,
-  18: true,
-  19: true,
-  20: false,
-  21: false,
-  22: false,
-  23: false
+  monday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  tuesday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  wednesday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  thursday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  friday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  }
 }
-
 export const addProvider = async (user: User) => {
   const snap = await getDoc(doc(db, "providers", user.uid))
   if (snap.exists()) throw new Error()
--- src/Profile/HoursOfOperations.tsx
+++ src/Profile/HoursOfOperations.tsx
@@ -1,13 +1,64 @@
-import { Checkbox, Col, Form, Row, TimePicker, Typography } from "antd"
+import { Col, Form, Row, Space, TimePicker, Typography } from "antd"
 import { User } from "../Auth/Model"
 
 const { Title } = Typography
+
+type Shifts = {
+  monday: {
+    available: boolean
+    shifts: { from: number; to: number }[]
+  }
+  tuesday: {
+    available: boolean
+    shifts: { from: number; to: number }[]
+  }
+  wednesday: {
+    available: boolean
+    shifts: { from: number; to: number }[]
+  }
+  thursday: {
+    available: boolean
+    shifts: { from: number; to: number }[]
+  }
+  friday: {
+    available: boolean
+    shifts: { from: number; to: number }[]
+  }
+}
+type Profile = {
+  name: string
+  email: string
+  phoneNumber: string
+}
+
+const shift: Shifts = {
+  monday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  tuesday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  wednesday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  thursday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  },
+  friday: {
+    available: true,
+    shifts: [{ from: 8, to: 12 }]
+  }
+}
 
 type Props = {
   user: User
 }
 export const HoursOfOperations = ({ user }: Props) => {
-  const [form] = Form.useForm()
+  const [form] = Form.useForm<Profile & Shifts>()
 
   return (
     <>
@@ -20,80 +71,77 @@ export const HoursOfOperations = ({ user }: Props) => {
           phoneNumber: user.phoneNumber
         }}
       >
-        <Row style={{ marginBottom: "1em", gap: "2em" }}>
-          <Col>
-            <Title level={4}>Lunes</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-          <Col>
-            <Title level={4}>Martes</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-          <Col>
-            <Title level={4}>Miercoles</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-          <Col>
-            <Title level={4}>Jueves</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-        </Row>
-        <Row style={{ gap: "2em" }}>
-          <Col>
-            <Title level={4}>Viernes</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-          <Col>
-            <Title level={4}>Sabado</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-          <Col>
-            <Title level={4}>Domingo</Title>
-            <TimePicker.RangePicker format="HH" />
-          </Col>
-        </Row>
+        {shift.monday.available ? (
+          <Row style={{ marginBottom: "1em" }}>
+            <Col span={3}>
+              <Title level={4}>Lunes</Title>
+            </Col>
+            <Space>
+              {shift.monday.shifts.map((slot, i) => (
+                <div key={`monday-slot-${i}`}>
+                  <TimePicker.RangePicker format="HH" />
+                </div>
+              ))}
+            </Space>
+          </Row>
+        ) : null}
+        {shift.tuesday.available ? (
+          <Row style={{ marginBottom: "1em" }}>
+            <Col span={3}>
+              <Title level={4}>Martes</Title>
+            </Col>
+            <Space>
+              {shift.tuesday.shifts.map((slot, i) => (
+                <div key={`tuesday-slot-${i}`}>
+                  <TimePicker.RangePicker format="HH" />
+                </div>
+              ))}
+            </Space>
+          </Row>
+        ) : null}
+        {shift.wednesday.available ? (
+          <Row style={{ marginBottom: "1em" }}>
+            <Col span={3}>
+              <Title level={4}>Miercoles</Title>
+            </Col>
+            <Space>
+              {shift.wednesday.shifts.map((slot, i) => (
+                <div key={`wednesday-slot-${i}`}>
+                  <TimePicker.RangePicker format="HH" />
+                </div>
+              ))}
+            </Space>
+          </Row>
+        ) : null}
+        {shift.thursday.available ? (
+          <Row style={{ marginBottom: "1em" }}>
+            <Col span={3}>
+              <Title level={4}>Jueves</Title>
+            </Col>
+            <Space>
+              {shift.thursday.shifts.map((slot, i) => (
+                <div key={`thursday-slot-${i}`}>
+                  <TimePicker.RangePicker format="HH" />
+                </div>
+              ))}
+            </Space>
+          </Row>
+        ) : null}
+        {shift.friday.available ? (
+          <Row style={{ marginBottom: "1em" }}>
+            <Col span={3}>
+              <Title level={4}>Viernes</Title>
+            </Col>
+            <Space>
+              {shift.friday.shifts.map((slot, i) => (
+                <div key={`friday-slot-${i}`}>
+                  <TimePicker.RangePicker format="HH" />
+                </div>
+              ))}
+            </Space>
+          </Row>
+        ) : null}
       </Form>
     </>
   )
 }
-
-/*
- <Form.Item name="checkbox-group" label="Checkbox.Group">
-        <Checkbox.Group>
-          <Row>
-            <Col >
-              <Checkbox value="A" style={{ lineHeight: '32px' }}>
-                A
-              </Checkbox>
-            </Col>
-            <Col >
-              <Checkbox value="B" style={{ lineHeight: '32px' }} disabled>
-                B
-              </Checkbox>
-            </Col>
-            <Col >
-              <Checkbox value="C" style={{ lineHeight: '32px' }}>
-                C
-              </Checkbox>
-            </Col>
-            <Col >
-              <Checkbox value="D" style={{ lineHeight: '32px' }}>
-                D
-              </Checkbox>
-            </Col>
-            <Col >
-              <Checkbox value="E" style={{ lineHeight: '32px' }}>
-                E
-              </Checkbox>
-            </Col>
-            <Col >
-              <Checkbox value="F" style={{ lineHeight: '32px' }}>
-                F
-              </Checkbox>
-            </Col>
-          </Row>
-        </Checkbox.Group>
-      </Form.Item>
-
-
-*/
